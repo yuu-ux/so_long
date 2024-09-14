36568e571dff5bbce679af526d93c594277c0daa
yuu-ux <yuugaehara@gmail.com>
2024-09-14 19:45:02 +0900

    feat: Loading and displaying images ==== Prefix ==== - feat: 新しい機能 - fix: バグの修正 - docs: ドキュメントのみの変更 - style: 空白、フォーマット、セミコロン追加など - refactor: 仕様に影響がないコード改善(リファクタ) - perf: パフォーマンス向上関連 - test: テスト関連 - chore: ビルド、補助ツール、ライブラリ関連 - wip: 一時中断

--- main.c
+++ main.c
@@ -1,12 +1,57 @@
 #include "./libmlx/mlx.h"
+#include <stdlib.h>
+#include <string.h>
 
-int main(void)
-{
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+typedef struct s_vars {
     void *mlx;
-    void *mlx_win;
+    void *win;
+}   t_vars;
 
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-    mlx_loop(mlx);
+void    my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+    char *dst;
+
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+
+int close(int keycode, t_vars *vars)
+{
+    if (keycode == 53)
+    {
+        mlx_destroy_window(vars->mlx, vars->win);
+        exit(0);
+    }
     return (0);
+}
+
+int close_window(t_vars *vars)
+{
+    exit(0);
+}
+
+int	main(void)
+{
+    t_vars vars;
+    char *img;
+    char *relative_path;
+    relative_path = strdup("./background.xpm");
+    int img_width;
+    int img_height;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1000, 650, "Hello world!");
+    img = mlx_xpm_file_to_image(vars.mlx, relative_path, &img_width, &img_height);
+    mlx_put_image_to_window(vars.mlx, vars.win, img, 0, 0);
+    mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+    mlx_hook(vars.win, 17, 1L<<17, close_window, &vars);
+	mlx_loop(vars.mlx);
 }
